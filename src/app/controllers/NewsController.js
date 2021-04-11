8edcaaaa3c0e243a7da1f4cb2d47070df1af53c4
hiradang <sunnybinhdang@gmail.com>
2021-04-11 22:13:28 +0700

    add prettier, lint-staged, husky

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,12 @@
+class NewController {
+    // [GET] /news
+    index(req, res) {
+        res.render('news');
+    }
+
+    show(req, res) {
+        res.send('NEW DETAILS !!!');
+    }
+}
+
+module.exports = new NewController();
